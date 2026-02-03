af3cd1f8315573b806fdcd02715abfa223a25bca
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-02-03 08:35:40 -0500

    use snap instead of bin for binary location

--- valorant/scraper.py
+++ valorant/scraper.py
@@ -9,11 +9,15 @@ from config import (
     ARTICLE_CONTENT_SELECTOR
 )
 
-options = Options()                                                                                                                                                                                                                               
-options.add_argument('--headless')                                                                                                                                                                                                                
-options.add_argument('--no-sandbox')                                                                                                                                                                                                              
+options = Options()
+options.add_argument('--headless=new')
+options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-options.binary_location = '/usr/bin/chromium-browser'
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-software-rasterizer')
+options.add_argument('--disable-extensions')
+options.add_argument('--remote-debugging-port=0')
+options.binary_location = '/snap/bin/chromium'
 service = Service('/usr/bin/chromedriver')
 
 def is_youtube_link(url):
