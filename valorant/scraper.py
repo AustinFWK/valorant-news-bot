db9475634ae354677fa2b4fe4e201b4e48375413
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2026-02-03 09:00:18 -0500

    update chromium and chrome flags

--- valorant/scraper.py
+++ valorant/scraper.py
@@ -10,18 +10,14 @@ from config import (
 )
 
 options = Options()
-options.add_argument('--headless=new')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('--disable-software-rasterizer')
-options.add_argument('--disable-extensions')
-options.add_argument('--disable-setuid-sandbox')
-options.add_argument('--no-zygote')
-options.add_argument('--single-process')
-options.add_argument('--user-data-dir=/tmp/chrome-data')
-options.binary_location = '/snap/bin/chromium'
-service = Service('/usr/bin/chromedriver')
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
+
+options.binary_location = "/usr/bin/chromium-browser"
+service = Service("/usr/bin/chromedriver")
 
 def is_youtube_link(url):
     """Check if a given URL is a YouTube link."""
